d25b3c768d01b86add20829fe7e26d73696e8da6
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-19 16:43:48 +0700

    update to kotlin 1.3.60

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,10 +9,13 @@ repositories {
     google()
 
     maven { url = uri("https://dl.bintray.com/icerockdev/plugins") }
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlin") }
 }
 
 dependencies {
-    implementation("dev.icerock:mobile-multiplatform:0.2.0")
+    implementation("dev.icerock:mobile-multiplatform:0.4.0")
+    implementation("com.android.tools.build:gradle:3.5.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60")
 }
 
 kotlinDslPluginOptions {
